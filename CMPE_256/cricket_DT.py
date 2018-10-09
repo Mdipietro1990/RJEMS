f7691c4af1ba0b66bfd6ba978957c58c3702068d
Matt DiPietro <mattsmacbook@Matts-MacBook-Pro.local>
2018-10-08 20:00:24 -0700

    fix

--- CMPE_256/cricket_DT.py
+++ CMPE_256/cricket_DT.py
@@ -1,19 +1,8 @@
-from IPython.display import Image
-from sklearn.tree import export_graphviz
-import pydotplus
-from sklearn.externals.six import StringIO
-from sklearn.tree import DecisionTreeClassifier
+from sklearn import tree
+import graphviz
 
+clf = tree.DecisionTreeClassifier()
 
-clf = DecisionTreeClassifier()
-
-'''
-with open('/Users/mattsmacbook/Downloads/playCricket.csv') as csv_file:
-    data_list = list(csv.reader(csv_file))
-    print data_list
-
-f = data_list[1:]
-'''
 X = [[1,2,3,4],
       [1,2,3,5],
       [6,2,3,4],
@@ -31,15 +20,12 @@ X = [[1,2,3,4],
 
 Y = ['not-play','not-play', 'play','play','play','not-play','play','not-play','play','play','play','play','play','not-play']
 
-dtree = clf.fit(X, Y)
+clf = clf.fit(X, Y)
 
-dot_data=StringIO()
-
-export_graphviz(dtree, out_file='dt_vi_tree.dot',
-                     feature_names=['Outlook', 'Temperature', 'Humidity', 'Wind'],
-                     class_names=['play', 'not-play'],filled=True, rounded=True)
-
-image = pydotplus.graph_from_dot_data(dot_data.getvalue())
-
-Image(image.create_png())
-dot_data.ge
+dot_data = tree.export_graphviz(clf, out_file=None,
+                         feature_names=['Outlook','Temperatue','Humidity','Wind'],
+                         class_names=['not-play','play'],
+                         filled=True, rounded=True,
+                         special_characters=True)
+graph = graphviz.Source(dot_data)
+graph
